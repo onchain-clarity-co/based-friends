f7d208b7b8f32b4f1c62539c0fac253fbf6abba6
ncale <nicholasbrodeur@outlook.com>
2024-02-27 12:28:32 -0700

    add session and auth kit wrappers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+
+import { getServerSession } from "next-auth";
+import SessionProvider from "@/components/SessionProvider";
+import AuthKitProvider from '@/components/AuthKitProvider';
+
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
@@ -11,24 +16,35 @@ export const metadata: Metadata = {
   description: "Discover what your social circle is doing onchain.",
 };
 
-export default function RootLayout({
+const config = {
+  relay: 'https://relay.farcaster.xyz',
+  rpcUrl: 'https://mainnet.optimism.io',
+  domain: 'localhost:4000',
+  siweUri: 'http://localhost:4000/',
+};
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const session = await getServerSession();
+
   return (
     <html lang="en">
       <body className={`${font.className} box-border flex flex-col`}>
+        <SessionProvider session={session}><AuthKitProvider config={config}>
 
-        {/* Header */}
-        <Header />
+          {/* Header */}
+          <Header />
 
-        {/* Body */}
-        {children}
+          {/* Body */}
+          {children}
 
-        {/* Footer */}
-        <Footer />
-        
+          {/* Footer */}
+          <Footer />
+        </AuthKitProvider></SessionProvider>
       </body>
     </html>
   );
