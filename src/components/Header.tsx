4e3626c3b8aa59fa056033a367961b181e89b478
ncale <nicholasbrodeur@outlook.com>
2024-02-27 12:29:29 -0700

    replace auth button with SIWF auth button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
+import AuthButton from "@/components/AuthButton";
+
 export default async function Header() {
-    return (
-        <header className="px-1 py-4 flex justify-between content-center bg-red-200">
-            <h1 className="text-3xl bg-green-50">Onchain Clarity</h1>
-            <button className="bg-green-50 align-middle">Connect Wallet</button>
-        </header>
-    )
+	return (
+		<header className="px-1 py-4 flex justify-between content-center">
+			<h1 className="text-3xl">Onchain Clarity</h1>
+			<AuthButton/>
+		</header>
+	)
 }
