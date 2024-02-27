b7d52663da180c11dcb37690b8d8768960337655
ncale <nicholasbrodeur@outlook.com>
2024-02-27 11:17:31 -0700

    update user card styles

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -2,22 +2,22 @@ import Image from "next/image";
 
 export default function UserCard( props: { pfpUrl: string, username: string, castTime: string, onchainTime: string } ) {
 	return (
-		<div className="w-32 bg-red-600">
+		<div className="bg-gray-200 w-72 mr-2 mb-2">
 			<div className="flex">
 				{/* pfp */}
-				<Image
+				{/* <Image
 					src={props.pfpUrl}
 					alt={`${props.username} profile picture`}
 					width={300}
 					height={300}
 					className="rounded-full"
-				/>
+				/> */}
 				{/* user info */}
-				<div>
-					<h3>{props.username}</h3>
-					<p>active?</p>
-					<p>casted {props.castTime}</p>
-					<p>onchain {props.onchainTime}</p>
+				<div className="p-2">
+					<h3 className="text-md font-bold leading-none">{props.username}</h3>
+					<p className="text-sm leading-none">active?</p>
+					<p className="text-sm leading-none">casted {props.castTime}</p>
+					<p className="text-sm leading-none">onchain {props.onchainTime}</p>
 				</div>
 			</div>
 		</div>
