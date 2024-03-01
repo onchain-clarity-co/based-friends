45710dce33f6c2f2af850a91e4bebb0b8fa953b5
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-03-01 13:40:32 -0700

    Update user card design (#13)

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -3,7 +3,7 @@ import { IconContext } from "react-icons";
 import { LuCircle } from "react-icons/lu";
 import { LuMessagesSquare } from "react-icons/lu";
 import { LuInfo } from "react-icons/lu";
-import { Tooltip } from "@nextui-org/react";
+import { Avatar, Tooltip } from "@nextui-org/react";
 
 export default function UserCard( props: { 
 		displayName: string
@@ -36,6 +36,7 @@ export default function UserCard( props: {
 		}
 	}
 	const castMillisDiff = Date.now() - new Date(props.castTime).getTime()
+	const isOnline = ((castMillisDiff / (1000*60)) < 10)
 	const castMsg = createCastMsg(castMillisDiff)
 
 	function createOnchainMsg(milliseconds: number) {
@@ -82,53 +83,53 @@ export default function UserCard( props: {
 	)
 
 	return (
-		<div className="bg-gray-200 rounded-md p-1.5 truncate flex justify-between">
+		<div className="bg-gray-200 rounded-md p-2 truncate flex justify-between">
 			{/* profile */}
 			<div className="flex items-center">
 				{/* pfp */}
 				{props.pfpUrl ? (
-					<Image 
-						src={props.pfpUrl} 
-						alt={`${props.username} profile picture`}
-						width={300}
-						height={300}
-						className="rounded-full max-w-12 max-h-12 w-12 h-12"
-					/>
+					<Avatar 
+						isBordered
+						src={props.pfpUrl}
+						size="lg" />
 				) : (
 					<></>
 				)}
 				{/* profile info */}
-				<div className="flex flex-col ml-1">
+				<div className="flex flex-col ml-2">
 					{/* display name */}
 					<h3 className="text-md leading-none cursor-default font-extrabold">{props.displayName}</h3>
 					{/* username */}
 					<div className="flex items-center">
-						<span className="text-sm leading-tight cursor-default">{props.username}</span>
-						{props.followsMe ? <span className="text-xs leading-none rounded-sm ml-1.5" style={{border: "1px solid black", padding: "1px"}}>follows you</span> : ''}
+						<span className="text-sm leading-none cursor-default">{props.username}</span>
+						{props.followsMe ? <span className="text-xs leading-none rounded-sm ml-1.5 cursor-default" style={{border: "1px solid black", padding: "1px"}}>follows you</span> : ''}
 					</div>
 					{/* online / active bar */}
-					<div className="flex items-center mb-0.5">
-						<IconContext.Provider value={{color: 'green', size:'8px'}}>
+					<div className="flex items-center">
+						{/* online button */}
+						<a href={`https://warpcast.com/${props.username}`} target="_blank">
 							<Tooltip content={castTooltip} size="sm" radius="sm" closeDelay={10} offset={0} placement="bottom-start">
-								<span className=""><LuCircle /></span>
+								{isOnline ? (
+									<span className="text-xs leading-none rounded-md px-1 bg-green-400 text-green-950 font-bold">online</span>
+								) : (
+									<span className="text-xs leading-none rounded-md px-1 bg-gray-300 text-gray-950 font-bold">offline</span>
+								)}
 							</Tooltip>
-						</IconContext.Provider>
-						<span className="text-xs leading-none cursor-default ml-0.5 mr-1">online</span>
-						<a href={`https://onceupon.gg/${props.onchainHash}`} target="_blank" className="flex items-center cursor-default">
+						</a>
+						{/* onchain button */}
+						<a href={`https://onceupon.gg/${props.onchainHash}`} target="_blank">
 							{isActiveOnchain ? (
-								<><IconContext.Provider value={{color: 'blue', size:'8px'}}>
-									<Tooltip content={baseTooltip} size="sm" radius="sm" closeDelay={10} offset={0} placement="bottom-start">
-										<span className=""><LuCircle /></span>
-									</Tooltip>
-								</IconContext.Provider>
-								<span className="text-xs leading-none cursor-default ml-0.5">active onchain</span></>
+								<Tooltip content={baseTooltip} size="sm" radius="sm" closeDelay={10} offset={0} placement="bottom-start">
+									<span className="text-xs leading-none ml-0.5 rounded-md px-1 bg-blue-400 text-blue-950 font-bold">
+										active onchain
+									</span>
+								</Tooltip>
 							) : (
-								<><IconContext.Provider value={{color: 'red', size:'8px'}}>
-									<Tooltip content={baseTooltip} size="sm" radius="sm" closeDelay={10} offset={0} placement="bottom-start">
-										<span className=""><LuCircle /></span>
-									</Tooltip>
-								</IconContext.Provider>
-								<span className="text-xs leading-none cursor-default ml-0.5">not active onchain</span></>
+								<Tooltip content={baseTooltip} size="sm" radius="sm" closeDelay={10} offset={0} placement="bottom-start">
+									<span className="text-xs leading-none ml-0.5 rounded-md px-1 bg-gray-300 text-gray-950 font-bold">
+										not active onchain
+									</span>
+								</Tooltip>
 							)}
 						</a>
 					</div>
