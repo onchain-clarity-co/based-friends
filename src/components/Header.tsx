29476f16a8cc59a0dffa0373b65a41c5d3fc3173
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-03-01 18:12:02 -0700

    Added branding (#14)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,17 @@
+import Image from "next/image";
 import AuthButton from "@/components/AuthButton";
 
 export default async function Header() {
 	return (
 		<header className="fixed top-0 z-50 w-full h-20 px-20 flex flex-col justify-end" style={{backgroundColor: '#313131'}}>
 			<div className="flex justify-between items-end pb-1">
-				<h1 className="text-5xl">
-					<span style={{color: '#2954ED'}}>Based</span> <span className="text-white">Friends</span>
-				</h1>
-				<div className="">
-					<AuthButton/>
-				</div>
+				<Image 
+					src="/BasedFriendsClear.png"
+					alt="based friends logo"
+					width={1018}
+					height={121}
+					className="h-12 w-auto" />
+				<AuthButton/>
 			</div>
 			<hr className="w-full mb-2"/>
 		</header>
